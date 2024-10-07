1a16db0741e78aa6e5fc7940fc07f5c53b7a5863
Hemasagar-T-G <hstg99mail@gmail.com>
2024-10-07 21:42:55 +0530

    Implemented Model Customer and Model Transaction

---
+++ SKSLS_WebApi/Model/Customer.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace SKSLS_WebApi.Model
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ SKSLS_WebApi/Model/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace SKSLS_WebApi.Model
+{
+    public class Transaction
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public TransactionType TransactionType { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+    }
+}
